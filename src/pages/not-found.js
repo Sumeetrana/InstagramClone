cc03c5c3a7970e793fb5933b1d2e911a19fbfff0
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-31 09:58:02 +0530

    feat: not-found page completed

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,8 +1,30 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import Layout from "../components/shared/Layout";
+import Link from "react-router-dom/Link";
 
 function NotFoundPage() {
-  return <Layout title="Page Not Found">NotFoundPage</Layout>;
+  return (
+    <Layout title="Page Not Found" marginTop={80}>
+      <Typography
+        variant="h5"
+        align="center"
+        paragraph
+        style={{ fontWeight: 600 }}
+      >
+        Sorry, this page isn't available
+      </Typography>
+      <Typography align="center">
+        The link you followed may be broken, or the page may have been removed.
+        <Link to="/">
+          {" "}
+          <Typography color="primary" component="span">
+            Go back to Instagram
+          </Typography>
+        </Link>
+      </Typography>
+    </Layout>
+  );
 }
 
 export default NotFoundPage;
