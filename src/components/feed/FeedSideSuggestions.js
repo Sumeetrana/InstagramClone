ee947f48a35906dda8c44a6c30dafe3542e16df2
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-31 14:30:54 +0530

    feat: Loading spinners for sidesuggestions & loadMorePosts added

--- src/components/feed/FeedSideSuggestions.js
+++ src/components/feed/FeedSideSuggestions.js
@@ -4,10 +4,11 @@ import { getDefaultUser } from "../../data";
 import { useFeedSideSuggestionsStyles } from "../../styles";
 import UserCard from "../shared/UserCard";
 import FollowButton from "../shared/FollowButton";
+import { LoadingIcon } from "../../icons";
 
 function FeedSideSuggestions() {
   const classes = useFeedSideSuggestionsStyles();
-
+  let loading = false;
   return (
     <article className={classes.article}>
       <Paper className={classes.paper}>
@@ -21,12 +22,16 @@ function FeedSideSuggestions() {
         >
           Suggestions For you
         </Typography>
-        {Array.from({ length: 5 }, () => getDefaultUser()).map((user) => (
-          <div key={user.id} className={classes.card}>
-            <UserCard user={user} />
-            <FollowButton side />
-          </div>
-        ))}
+        {loading ? (
+          <LoadingIcon />
+        ) : (
+          Array.from({ length: 5 }, () => getDefaultUser()).map((user) => (
+            <div key={user.id} className={classes.card}>
+              <UserCard user={user} />
+              <FollowButton side />
+            </div>
+          ))
+        )}
       </Paper>
     </article>
   );
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFeedPageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
 import UserCard from "../components/shared/UserCard";
@@ -6,9 +6,16 @@ import { getDefaultPost } from "../data";
 import FeedPost from "../components/feed/FeedPost";
 import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
 import { Hidden } from "@material-ui/core";
+import LoadingScreen from "../components/shared/LoadingScreen";
+import { LoadingLargeIcon } from "../icons";
 
 function FeedPage() {
   const classes = useFeedPageStyles();
+  const [isEndOfFeed] = useState(false);
+  let loading = false;
+  if (loading) {
+    return <LoadingScreen />;
+  }
 
   return (
     <Layout>
@@ -28,6 +35,7 @@ function FeedPage() {
             </div>
           </div>
         </Hidden>
+        {!isEndOfFeed && <LoadingLargeIcon />}
       </div>
     </Layout>
   );
