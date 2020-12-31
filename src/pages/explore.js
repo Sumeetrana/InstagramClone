796734b017c06dac85ee33bc7f9748abf587784e
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-31 18:20:54 +0530

    feat: adding Layout shared component to all other pages

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Layout from "../components/shared/Layout";
 
 function ExplorePage() {
-  return <div>ExplorePage</div>;
+  return <Layout>Explore page</Layout>;
 }
 
 export default ExplorePage;
