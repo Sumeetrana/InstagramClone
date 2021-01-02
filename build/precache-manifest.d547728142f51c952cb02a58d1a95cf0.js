7e586d00443d9279f0232a0a49a78791942baabc
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-02 21:39:15 +0530

    build: npm run build

---
+++ build/precache-manifest.d547728142f51c952cb02a58d1a95cf0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5aaa015ef14327395215d3a957dce27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19e466de2074690da1e",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "c19e466de2074690da1e",
+    "url": "/static/js/2.a7b74719.chunk.js"
+  },
+  {
+    "revision": "e8ccc7cf4c855a56e3a7079a2b405db4",
+    "url": "/static/js/2.a7b74719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adba48250f2b3a962082",
+    "url": "/static/js/3.f9f58910.chunk.js"
+  },
+  {
+    "revision": "65ca6c796db6113da80c",
+    "url": "/static/js/main.6ab6b79d.chunk.js"
+  },
+  {
+    "revision": "b24461e29e2e52b97eef",
+    "url": "/static/js/runtime-main.dda5a51a.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
