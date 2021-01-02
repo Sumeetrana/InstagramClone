fe465464e3956b237d86b580e34b9b2215ec9ea1
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-02 20:07:03 +0530

    feat: edit-profile page setup

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,10 +1,101 @@
 import React from "react";
 import { useEditProfilePageStyles } from "../styles";
+import Layout from "../components/shared/Layout";
+import {
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Menu,
+} from "@material-ui/core";
 
-function EditProfilePage() {
-  useEditProfilePageStyles();
+function EditProfilePage({ history }) {
+  const classes = useEditProfilePageStyles();
+  const [showDrawer, setDrawer] = React.useState(false);
 
-  return <div>EditProfilePage</div>;
+  function handleToggleDrawer() {
+    setDrawer((prev) => !prev);
+  }
+
+  function handleSelected(index) {
+    const path = history.location.pathname;
+    switch (index) {
+      case 0:
+        return path.includes("edit");
+      default:
+        break;
+    }
+  }
+
+  function handleListClick(index) {
+    switch (index) {
+      case 0:
+        history.push("/accounts/edit");
+        break;
+      default:
+        break;
+    }
+  }
+
+  const options = [
+    "Edit Profile",
+    "Change Password",
+    "Apps and Websites",
+    "Emails and SMS",
+    "Push Notifications",
+    "Manage Contact",
+    "Privacy and Security",
+    "Login Activity",
+    "Emails from Instagram",
+  ];
+
+  const drawer = (
+    <List>
+      {options.map((option, index) => (
+        <ListItem
+          key={option}
+          button
+          selected={handleSelected(index)}
+          onClick={() => handleListClick(index)}
+          classes={{
+            selected: classes.listItemSelected,
+            button: classes.listItemButton,
+          }}
+        >
+          <ListItemText primary={option} />
+        </ListItem>
+      ))}
+    </List>
+  );
+
+  return (
+    <Layout title="Edit Profile">
+      <section className={classes.section}>
+        <IconButton
+          edge="start"
+          onClick={handleToggleDrawer}
+          className={classes.menuButton}
+        >
+          <Menu />
+        </IconButton>
+        <nav>
+          <Hidden smUp implementation="css">
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={showDrawer}
+              onClose={handleToggleDrawer}
+              classes={{ paperAnchorLeft: classes.temporaryDrawer }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+      </section>
+    </Layout>
+  );
 }
 
 export default EditProfilePage;
