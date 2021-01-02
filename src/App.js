17adb55fef9c818ba9569905decbc91a4ece4b7f
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-02 11:53:59 +0530

    feat: GridPost Modal#2(Integrating Router on upper level of App component, on index.js file)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import FeedPage from "./pages/feed";
 import ProfilePage from "./pages/profile";
 import PostPage from "./pages/post";
@@ -8,21 +8,20 @@ import EditProfilePage from "./pages/edit-profile";
 import LoginPage from "./pages/login";
 import SignupPage from "./pages/signup";
 import NotFoundPage from "./pages/not-found";
+import PostModal from "./components/post/PostModal";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={FeedPage} />
-        <Route path="/explore" component={ExplorePage} />
-        <Route exact path="/:username" component={ProfilePage} />
-        <Route exact path="/p/:postId" component={PostPage} />
-        <Route path="/accounts/edit" component={EditProfilePage} />
-        <Route path="/accounts/login" component={LoginPage} />
-        <Route path="/accounts/emailsignup" component={SignupPage} />
-        <Route path="*" component={NotFoundPage} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={FeedPage} />
+      <Route path="/explore" component={ExplorePage} />
+      <Route exact path="/:username" component={ProfilePage} />
+      <Route exact path="/p/:postId" component={PostPage} />
+      <Route path="/accounts/edit" component={EditProfilePage} />
+      <Route path="/accounts/login" component={LoginPage} />
+      <Route path="/accounts/emailsignup" component={SignupPage} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
   );
 }
 
