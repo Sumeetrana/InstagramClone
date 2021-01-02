26d65236b2ca109c383577cea6c252323335a6b8
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-02 12:09:46 +0530

    feat: GridPost Modal#3(High level code to enable modal routing in App.js)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useHistory, useLocation } from "react-router-dom";
 import FeedPage from "./pages/feed";
 import ProfilePage from "./pages/profile";
 import PostPage from "./pages/post";
@@ -11,17 +11,33 @@ import NotFoundPage from "./pages/not-found";
 import PostModal from "./components/post/PostModal";
 
 function App() {
+  const history = useHistory();
+  const location = useLocation();
+  const prevLocation = React.useRef(location);
+  const modal = location.state?.modal;
+
+  React.useEffect(() => {
+    if (history.action !== "POP" && !modal) {
+      prevLocation.current = location;
+    }
+  }, [location, modal, history.action]);
+
+  const isModalOpen = modal && prevLocation.current !== location;
+
   return (
-    <Switch>
-      <Route exact path="/" component={FeedPage} />
-      <Route path="/explore" component={ExplorePage} />
-      <Route exact path="/:username" component={ProfilePage} />
-      <Route exact path="/p/:postId" component={PostPage} />
-      <Route path="/accounts/edit" component={EditProfilePage} />
-      <Route path="/accounts/login" component={LoginPage} />
-      <Route path="/accounts/emailsignup" component={SignupPage} />
-      <Route path="*" component={NotFoundPage} />
-    </Switch>
+    <>
+      <Switch location={isModalOpen ? prevLocation.current : location}>
+        <Route exact path="/" component={FeedPage} />
+        <Route path="/explore" component={ExplorePage} />
+        <Route exact path="/:username" component={ProfilePage} />
+        <Route exact path="/p/:postId" component={PostPage} />
+        <Route path="/accounts/edit" component={EditProfilePage} />
+        <Route path="/accounts/login" component={LoginPage} />
+        <Route path="/accounts/emailsignup" component={SignupPage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+      {isModalOpen && <Route exact path="/p/:pathId" component={PostModal} />}
+    </>
   );
 }
 
