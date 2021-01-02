a275159d3d0eb4fbbdeaf256473180ff058c8c5f
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-02 15:27:02 +0530

    feat: Profile picture component completed

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,3 +1,4 @@
+import { Person } from "@material-ui/icons";
 import React from "react";
 import { useProfilePictureStyles } from "../../styles";
 
@@ -6,9 +7,21 @@ function ProfilePicture({
   image = "https://images.unsplash.com/photo-1609518726036-d683cc25eef4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=410&q=80",
   isOwner,
 }) {
-  const classes = useProfilePictureStyles();
+  const classes = useProfilePictureStyles({ size, isOwner });
 
-  return <div>ProfilePicture</div>;
+  return (
+    <section className={classes.section}>
+      {image ? (
+        <div className={classes.wrapper}>
+          <img src={image} alt="user profile" className={classes.image} />
+        </div>
+      ) : (
+        <div className={classes.wrapper}>
+          <Person className={classes.person} />
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default ProfilePicture;
