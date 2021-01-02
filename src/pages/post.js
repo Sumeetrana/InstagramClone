200f11553c3a4bddeb35ededfc617e122da523e8
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-02 13:29:05 +0530

    feat: Individual Post Page setup

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../components/shared/Layout";
+import Post from "../components/post/Post";
+import MorePostsFromUser from "../components/post/MorePostsFromUser";
 
 function PostPage() {
-  return <div>PostPage</div>;
+  const { postId } = useParams();
+  return (
+    <Layout>
+      <Post id={postId}></Post>
+      <MorePostsFromUser />
+    </Layout>
+  );
 }
 
 export default PostPage;
