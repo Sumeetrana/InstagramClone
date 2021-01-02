e70a88f148f0aaaf12c3004b3116568651e05cb0
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-02 10:18:04 +0530

    feat: Follow suggestions on feed page completed

--- src/components/shared/FollowSuggestions.js
+++ src/components/shared/FollowSuggestions.js
@@ -1,10 +1,89 @@
+import { Avatar, Typography } from "@material-ui/core";
 import React from "react";
+import { LoadingLargeIcon } from "../../icons";
 import { useFollowSuggestionsStyles } from "../../styles";
+import Slick from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { getDefaultUser } from "../../data";
+import { Link } from "react-router-dom";
+import FollowButton from "./FollowButton";
 
 function FollowSuggestions() {
-  useFollowSuggestionsStyles();
+  const classes = useFollowSuggestionsStyles();
+  let loading = false;
 
-  return <div>FollowSuggestions</div>;
+  return (
+    <div className={classes.container}>
+      <Typography
+        color="textSecondary"
+        variant="subtitle2"
+        className={classes.typography}
+      >
+        Suggestions for you
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <Slick
+          className={classes.slide}
+          dots={false}
+          infinite
+          speed={1000}
+          touchThresold={1000}
+          variableWidth
+          swipeToSlide
+          arrows
+          slidesToScroll={3}
+          easing="ease-in-out"
+        >
+          {Array.from({ length: 10 }, () => getDefaultUser()).map((user) => (
+            <FollowSuggestionsItem key={user.id} user={user} />
+          ))}
+        </Slick>
+      )}
+    </div>
+  );
+}
+
+function FollowSuggestionsItem({ user }) {
+  const classes = useFollowSuggestionsStyles();
+  const { profile_image, username, name } = user;
+
+  return (
+    <div>
+      <div className={classes.card}>
+        <Link to={`/${username}`}>
+          <Avatar
+            src={profile_image}
+            alt={`${username}'s profile`}
+            classes={{
+              root: classes.avatar,
+              img: classes.avatarImg,
+            }}
+          />
+        </Link>
+        <Link to={`/${username}`}>
+          <Typography
+            variant="subtitle2"
+            className={classes.text}
+            align="center"
+          >
+            {username}
+          </Typography>
+        </Link>
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          className={classes.text}
+          align="center"
+        >
+          {name}
+        </Typography>
+        <FollowButton side={false} />
+      </div>
+    </div>
+  );
 }
 
 export default FollowSuggestions;
