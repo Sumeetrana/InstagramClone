f32bea282b23ec857a194e033dda149ab8bba028
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-05 12:01:02 +0530

    feat: mutation(EDIT_USER_AVATAR) created

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -53,3 +53,14 @@ export const EDIT_USER = gql`
     }
   }
 `;
+
+export const EDIT_USER_AVATAR = gql`
+  mutation editUser($id: uuid!, $profileImage: String!) {
+    update_users(
+      where: { id: { _eq: $id } }
+      _set: { profile_image: $profileImage }
+    ) {
+      affected_rows
+    }
+  }
+`;
