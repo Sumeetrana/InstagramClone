a40dfede656de862f8bba25d2e179ac6756f3788
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-05 16:17:51 +0530

    slate package added & styles for AddPostDialog component completed

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -1,5 +1,22 @@
 import React from "react";
+import {createEditor} from "slate";
+import {Slate, Editable, withReact} from "slate-react";
+import {useAddPostDialogStyles} from '../../styles'
+
+
+const initialValue = [
+    {
+        type: "paragraph",
+        children: [{text: ""}]
+    }
+]
 
 export default function AddPostDialog({ media, handleClose }) {
-  return <div></div>;
+    const classes = useAddPostDialogStyles()
+    const editor = React.useMemo(() => withReact(createEditor()), [])
+    const [value, setValue] = React.useState(initialValue)
+
+  return (
+    
+  );
 }
