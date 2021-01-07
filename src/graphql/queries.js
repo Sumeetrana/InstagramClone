a1ddc50d726020043190aca83d65dd8a1169f3b2
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-07 10:37:19 +0530

    feat: query(SUGGEST_USERS) added to get suggestion for users

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -107,3 +107,27 @@ export const GET_USER_PROFILE = gql`
     }
   }
 `;
+
+// suggest users from followers & also users creted around the same time
+export const SUGGEST_USERS = gql`
+  query suggestUsers(
+    $limit: Int!
+    $followerIds: [uuid!]!
+    $createdAt: timestamptz!
+  ) {
+    users(
+      limit: $limit
+      where: {
+        _or: [
+          { id: { _in: $followerIds } }
+          { created_at: { _gt: $createdAt } }
+        ]
+      }
+    ) {
+      id
+      username
+      name
+      profile_image
+    }
+  }
+`;
