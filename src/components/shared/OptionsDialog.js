e1791f748f97a2ec3b114d57f780225b0e014805
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-07 14:00:24 +0530

    feat: unfollowing the user on directly by post

--- src/components/shared/OptionsDialog.js
+++ src/components/shared/OptionsDialog.js
@@ -3,9 +3,30 @@ import React from "react";
 import { useOptionsDialogStyles } from "../../styles";
 import { Link } from "react-router-dom";
 import { defaultPost } from "../../data";
+import { UserContext } from "../../App";
+import { UNFOLLOW_USER } from "../../graphql/mutations";
+import { useMutation } from "@apollo/react-hooks";
 
-function OptionsDialog({ onClose }) {
+function OptionsDialog({ onClose, postId, authorId }) {
   const classes = useOptionsDialogStyles();
+  const { currentUserId, followingIds } = React.useContext(UserContext);
+  const isOwner = authorId === currentUserId;
+  const buttonText = isOwner ? "Delete" : "Unfollow";
+  const onClick = isOwner ? handleDeletePost : handleUnfollowUser;
+  const isFollowing = followingIds.some((id) => id === authorId);
+  const isUnrelatedUser = !isOwner && !isFollowing;
+  const [unfollowUser] = useMutation(UNFOLLOW_USER);
+
+  function handleDeletePost() {}
+
+  function handleUnfollowUser() {
+    const variables = {
+      userIdToFollow: authorId,
+      currentUserId,
+    };
+    unfollowUser({ variables });
+    onClose();
+  }
 
   return (
     <Dialog
@@ -16,7 +37,9 @@ function OptionsDialog({ onClose }) {
       onClose={onClose}
       TransitionComponent={Zoom}
     >
-      <Button className={classes.redButton}>Unfollow</Button>
+      {!isUnrelatedUser && (
+        <Button className={classes.redButton}>Unfollow</Button>
+      )}
       <Divider />
       <Button className={classes.button}>
         <Link to={`/p/${defaultPost.id}`}>Go to post</Link>
