4849d97250054268c4e9dcbf5b90e54359f47ee5
Sumeetrana <sumeetsinh28@gmail.com>
2021-01-07 22:53:29 +0530

    feat: Setting up feed page for new users

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -5,7 +5,7 @@ import UserCard from "../components/shared/UserCard";
 // import { getDefaultPost } from "../data";
 // import FeedPost from "../components/feed/FeedPost";
 import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
-import { Hidden } from "@material-ui/core";
+import { Hidden, Typography } from "@material-ui/core";
 import LoadingScreen from "../components/shared/LoadingScreen";
 import { LoadingLargeIcon } from "../icons";
 import FeedPostSkeleton from "../components/feed/FeedPostSkeleton";
@@ -13,6 +13,8 @@ import { UserContext } from "../App";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_FEED } from "../graphql/queries";
 import usePageBottom from "../utils/usePageBottom";
+import FollowSuggestions from "../components/shared/FollowSuggestions";
+import { Add } from "@material-ui/icons";
 
 const FeedPost = React.lazy(() => import("../components/feed/FeedPost"));
 
@@ -47,6 +49,40 @@ function FeedPage() {
       updateQuery: handleUpdateQuery,
     });
   }, [isPageBottom, data, handleUpdateQuery, fetchMore, feedIds]);
+
+  if (data && data.posts.length === 0) {
+    return (
+      <Layout>
+        <div
+          className={classes.container}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <Typography
+            variant="body1"
+            component="h1"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: "900px",
+              width: "100%",
+              margin: "50px auto",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Add style={{ fontSize: "50px" }} />
+            <Typography varinat="body2" component="h2">
+              Start Following & Start Sharing.
+            </Typography>
+            <Typography variant="subtitle2" component="span">
+              Write Your Own Story
+            </Typography>
+          </Typography>
+          <FollowSuggestions />
+        </div>
+      </Layout>
+    );
+  }
 
   if (loading) {
     return <LoadingScreen />;
